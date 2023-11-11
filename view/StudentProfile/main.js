81fe56f5d8d5c691f54acc244eeb7c5ff68e25b6
plaze <banikakash131@gamil.com>
2023-11-11 13:06:17 +0530

    feat: add teacherProfile

--- view/StudentProfile/main.js
+++ view/StudentProfile/main.js
@@ -3,10 +3,11 @@ const profileCard = document.getElementById("profileCard");
 const update = async () => {
   let data = await (await fetch("./../../model/data.json")).json();
   data.forEach((card) => {
-    profileCard.innerHTML += ` <div class="slot">
-        <p class="slot__name">${card.name}</p>
-        <button class="slot__button">Join Now</button>
-      </div>`;
+    profileCard.innerHTML += `  <div class="slot">
+          <p class="slot__name">${card.name}</p>
+          <input type="text" name="meet" id="meet" class="slot__meet" />
+          <button class="slot__button">Join Now</button>
+        </div>`;
   });
 };
 update();
